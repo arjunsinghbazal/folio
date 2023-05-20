f53038a4a32fcee0e759bb9b96524fb037cee03a
Arjun Singh <arjunsinghbazal@gmail.com>
2023-05-20 22:43:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+function toggleSkillsHeading() {
+  var skillsHeading = document.getElementById('skills-heading');
+  var currentText = skillsHeading.innerText;
+  var newText = (currentText === 'Web Developer') ? 'Programmer' : 'Web Developer';
+  skillsHeading.innerText = newText;
+}
+
+// Set the interval to toggle the heading text every 2 seconds
+setInterval(toggleSkillsHeading, 1500);
+
+function toggleContactForm() {
+  var contactFormContainer = document.getElementById("contact-form-container");
+  var container = document.querySelector(".container");
+
+  if (contactFormContainer.style.display === "none") {
+    contactFormContainer.style.display = "block";
+    container.style.display = "none";
+  } else {
+    if (nameInput === "" && emailInput === "" && messageInput === "") {
+      contactFormContainer.style.display = "none";
+      container.style.display = "block";
+    }
+  }
+}
+
+function backit() {
+  var contactFormContainer = document.getElementById("contact-form-container");
+  var container = document.querySelector(".container");
+
+  contactFormContainer.style.display = "none";
+  container.style.display = "block";
+}
